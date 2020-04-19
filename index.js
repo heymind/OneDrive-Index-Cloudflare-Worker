04ebd3f54c330cc1b3ce40d677a06150398f03b1
微影 <48644801+Micraow@users.noreply.github.com>
2020-04-19 09:28:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -57,10 +57,49 @@ const config = {
      */
     "proxyDownload": true,
 };
+setTimeout(clean(),0);
 
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
 })
+/**
+*Clean the cache and access token every 8 minutes
+*/
+function clean()
+{
+    /**
+ * Current access token 
+ */
+let _accessToken = null;
+
+/**
+ * Cloudflare cache instance
+ */
+let cache = caches.default;
+    /**
+ * Get access token for microsoft graph API endpoints. Refresh token if needed.
+ */
+async function getAccessToken() {
+    if (_accessToken) return _accessToken;
+    resp = await fetch("https://login.microsoftonline.com/common/oauth2/v2.0/token", {
+        method: "POST",
+        body: `client_id=${config.client_id}&redirect_uri=${config.redirect_uri}&client_secret=${config.client_secret}
+    &refresh_token=${config.refresh_token}&grant_type=refresh_token`,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+    });
+    if (resp.ok) {
+        console.info("access_token refresh success.")
+        const data = await resp.json()
+        _accessToken = data.access_token
+        return _accessToken;
+    } else throw `getAccessToken error ${ JSON.stringify(await resp.text())}`
+}
+}
+var int=self.setInterval("clean()",480000);
+
+    
 
 /**
  * Current access token 
